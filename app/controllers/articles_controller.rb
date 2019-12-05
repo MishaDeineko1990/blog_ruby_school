d99001b5f3acbf3d0f838803b27cff54be826e25
MishaDeineko1990 <deinekomisha1990@gmail.com>
2019-12-06 00:26:33 +0200

    iplement aticle create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,8 +5,19 @@ def new
 end
 
 def create
-  
+    @article = Article.new(article_params)
+    
+    if  @article.valid?
+        @article.save
+    else
+      render action: 'new'
+    end
 end
 
+private
+
+def article_params
+  params.require(:article).permit(:title, :text)
+end
 
 end
